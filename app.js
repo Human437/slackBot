ec241c622066ecc00c341efcbb20a642d545229a
Ryan <leeyuenhigh@gmail.com>
2021-09-11 22:40:48 -0400

    Initial basic slack bot

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-const express = require("express");
 const dotenv = require("dotenv");
+const { App } = require("@slack/bolt");
 
 dotenv.config();
 
-const clientId = process.env.clientId;
-const clientSecret = process.env.clientSecret;
+const bot = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
 
-const app = express();
+// Listens to incoming messages that contain "hello"
+bot.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
+
+bot.action("button_click", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
 
 const PORT = process.env.PORT || 4390;
 
-app.listen(PORT, () => {
-  console.log(`Server on http://localhost:${PORT}`);
-});
+(async () => {
+  // Start your app
+  await bot.start(process.env.PORT || 4390);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  console.log("⚡️ Bolt app is running!");
+})();
