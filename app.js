618e65edffe1e55dc74a912c952050ecc6fc1b57
Ryan <leeyuenhigh@gmail.com>
2021-09-13 22:36:52 -0400

    Added weather functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const { App } = require("@slack/bolt");
+const axios = require("axios");
 
 dotenv.config();
 
@@ -40,6 +41,71 @@ bot.action("button_click", async ({ body, ack, say }) => {
 });
 
 const PORT = process.env.PORT || 4390;
+
+bot.message(/weather/i, async ({ message, say }) => {
+  let city = message.text.split(" ")[1];
+  if (city) {
+    try {
+      const res = await axios.get(
+        "https://api.openweathermap.org/data/2.5/weather",
+        {
+          params: {
+            q: city,
+            appid: process.env.WEATHER_TOKEN,
+          },
+        }
+      );
+      if (res.status === 200) {
+        let weatherData = res.data;
+        let temp = (
+          ((Number(weatherData.main.temp) - 273.15) * 9) / 5 +
+          32
+        ).toFixed(2);
+        let generalText = `Temperature: ${temp}°F \n Pressure: ${weatherData.main.pressure} \n Humidity: ${weatherData.main.humidity}`;
+        let windText = `Speed: ${weatherData.wind.speed}\n Degree: ${weatherData.wind.deg}`;
+        const message = {
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*General*",
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: generalText,
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*Wind*",
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: windText,
+              },
+            },
+          ],
+        };
+        await say(message);
+      } else {
+        await say(res.statusText);
+      }
+    } catch (err) {
+      // console.log("error:", err);
+    }
+  } else {
+    await say("You may want to try this: weather {city name}");
+  }
+});
 
 (async () => {
   // Start your app
